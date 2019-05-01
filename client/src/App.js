649e50540460b10bf690986dbdd8ce560c00bafe
kareem da <kareemda1@gmail.com>
2019-05-01 16:12:51 +0300

    removed unnecessary files

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Customers from './components/customers';
 
@@ -9,7 +8,6 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">React Express Starter</h1>
         </header>
         <Customers />
