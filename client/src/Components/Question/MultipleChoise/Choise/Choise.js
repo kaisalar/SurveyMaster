fb946ddf8e9c5e5b056a7128435983cf7d8ab835
kareem da <kareemda1@gmail.com>
2019-05-04 11:06:49 +0300

    placed every dependency in its place on package.json root - client

--- client/src/Components/Question/MultipleChoise/Choise/Choise.js
+++ client/src/Components/Question/MultipleChoise/Choise/Choise.js
@@ -1,23 +1,51 @@
-import React from 'react';
+import React, { Component } from "react";
 import { MDBInput, MDBBtn } from "mdbreact";
-import styleClass from './Choice.module.css'
+import styleClass from "./Choice.module.css";
 
+class Choise extends Component {
+  state = {
+    mouseHover: false
+  };
+  mouseHoverOn = () => {
+    console.log('mousein');
+    this.setState({
+      mouseHover: true
+    });
+  };
+  mouseHoverOff = () => {
+    this.setState({
+      mouseHover: false
+    });
+  };
 
-const Choise = props => {
-    return(
-        <div className={styleClass.Choice}>
-            <MDBInput
-          label={props.el}
-          value={props.el}
-          onKeyPress={event => props.keyPressedHandler(event)}
-          onChange={e =>
-            props.changeChoiseHandler(e.target.value)
-          }
+  render() {
+    let Style = null;
+    if(this.state.mouseHover){
+      Style = { 
+        borderBottom: "1px solid #D32F2F",
+        boxShadow: '0 1px 0 0 #D32F2F'
+      }
+    }
+    return (
+      <div className={styleClass.Choice}>
+        <MDBInput
+          label={this.props.el}
+          style={Style}
+          value={this.props.el}
+          onKeyPress={event => this.props.keyPressedHandler(event)}
+          onChange={e => this.props.changeChoiseHandler(e.target.value)}
         />
-        <MDBBtn color="red" onClick={props.clicked}>Delete</MDBBtn>
-        </div>
-        
+        <MDBBtn
+          color="red"
+          onClick={this.props.clicked}
+          onMouseEnter={this.mouseHoverOn}
+          onMouseLeave={this.mouseHoverOff}
+        >
+          Delete
+        </MDBBtn>
+      </div>
     );
+  }
 }
 
-export default Choise
+export default Choise;
