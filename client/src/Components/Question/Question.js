ff8d63753b454070c91d391bbd1aea92abf11899
kareem da <kareemda1@gmail.com>
2019-05-05 14:10:53 +0300

    Added rsuite Select Picker

--- client/src/Components/Question/Question.js
+++ client/src/Components/Question/Question.js
@@ -6,6 +6,7 @@ import MultipleChoice from "./MultipleChoise/MultipleChoice";
 import { connect } from "react-redux";
 import * as actions from "../../store/actions/types";
 import * as Qtypes from "./QuestionTypes";
+import {  SelectPicker } from "rsuite";
 
 class Question extends Component {
   render() {
@@ -25,6 +26,12 @@ class Question extends Component {
         AnswerType = <ShortText index={index} />;
         break;
     }
+    const data = [
+      {label: "Short Text", value: Qtypes.TEXT},
+      {label: Qtypes.MULTIPLE_CHOISE, value: Qtypes.MULTIPLE_CHOISE},
+      {label: Qtypes.RADIO_GROUP, value: Qtypes.RADIO_GROUP},
+      {label: Qtypes.DROPDOWN_MENU, value: Qtypes.DROPDOWN_MENU},
+    ]
     return (
       <div className={styleClass.QuestionContainer}>
         <div className={styleClass.Question}>
@@ -34,16 +41,22 @@ class Question extends Component {
             onChange={e => this.props.ChangeLabelHandler(index, e.target.value)}
             className={styleClass.BigText}
           />
-          <select
+          <SelectPicker
             className={styleClass.SelectInput}
+            data={data}
+            defaultValue={Qtypes.TEXT}
             value={Q.type}
-            onChange={e => this.props.ChangeTypeHandler(index, e.target.value)}
+            searchable={false}
+            cleanable={false}
+            onChange={newVal =>
+              this.props.ChangeTypeHandler(index, newVal)
+            }
           >
-            <option value={Qtypes.TEXT}>short answer</option>
+            {/* <option value={Qtypes.TEXT}>short answer</option>
             <option value={Qtypes.MULTIPLE_CHOISE}>multiple choise</option>
             <option value={Qtypes.RADIO_GROUP}>radio Group</option>
-            <option value={Qtypes.DROPDOWN_MENU}>Dropdown Menu</option>
-          </select>
+            <option value={Qtypes.DROPDOWN_MENU}>Dropdown Menu</option> */}
+          </SelectPicker>
         </div>
         <div className={styleClass.Answer}>{AnswerType}</div>
       </div>
