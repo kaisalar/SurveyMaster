0d8ec0c0df648c20669730436ef2f086c81ab5c3
kareem da <kareemda1@gmail.com>
2019-05-05 16:10:03 +0300

    cached options when changing types

--- client/src/store/reducers/createSurveyReducer.js
+++ client/src/store/reducers/createSurveyReducer.js
@@ -11,7 +11,9 @@ const reducer = (state = initialState, action) => {
         _id: state.Questions.length + 1,
         type: Qtypes.TEXT,
         title: "Untitled Question",
-        content: {}
+        content: {
+          choices: ["Option 1"]
+        }
       };
       return {
         ...state,
@@ -27,7 +29,7 @@ const reducer = (state = initialState, action) => {
     case actions.CHANGE_QUESTION_TYPE:
       newQuestions = [...state.Questions];
       newQuestions[action.index].type = action.val;
-      handleContentOfType(newQuestions[action.index]);
+     // handleContentOfType(newQuestions[action.index]);
       return {
         ...state,
         Questions: newQuestions
