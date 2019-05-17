4f5dbf919c652dd9ff2d9747811c6f263cee871a
Ammar0ah <ammar-hl@hotmail.com>
2019-05-17 22:13:00 +0300

    updating users

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import {BrowserRouter} from 'react-router-dom'
 import Routes from './Routes'
-
+import Signup from './Components/Sign Up/Signup'
 class App extends Component {
   render() {
     return (
       <BrowserRouter basename="/">
-        <div>
-          <Routes />
+        <div style={{maxWidth: "50%"}}>
+          {/* <Routes /> */}
+          <Signup/>
         </div>
       </BrowserRouter>
     );
--- client/src/store/actions/viewAction.js
+++ client/src/store/actions/viewAction.js
@@ -1,5 +1,18 @@
 import * as ActionType from "./types";
 import axios from "../../axios-requests";
+import {Alert} from 'rsuite'
+axios.interceptors.response(null, error => {
+  const expectedError =
+    error.response &&
+    error.response.status >= 400 &&
+    error.response.status < 500;
+    if(!expectedError)
+     {
+       console.log("Logging the error" ,error)
+       
+
+     }
+});
 export const setSurveys = surveys => {
   return {
     type: ActionType.SHOW_SURVEYS,
@@ -8,11 +21,9 @@ export const setSurveys = surveys => {
 };
 ////////////////////////
 export const deleteSurvey = id => dispatch => {
-  axios.delete("api/surveys/" + id)
-       .then(response => {
-
-              dispatch(initSurvey());
-              });
+  axios.delete("api/surveys/" + id).then(response => {
+    dispatch(initSurvey());
+  });
 };
 ////////////////////////
 export const initSurvey = () => dispatch => {
