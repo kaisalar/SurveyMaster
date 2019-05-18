b7eac3e025e810072d66d00d920368e537173c3a
kaisalar <kais.alar@outlook.com>
2019-05-18 14:10:51 +0300

    fix import bug

--- server/middlewares/admin.js
+++ server/middlewares/admin.js
@@ -1,11 +1,13 @@
 const User = require('../models/user')
 
-module.exports = (req, res, next) => {
-    const surveyId = req.params.id
-    const user = await User.findById(req.user._id)
-    
-    if (!user.isAdminOnSurvey(surveyId)) 
-        return res.status(403).send('Access denied. this user is not an admin on this survey')
-    
-    next()
+module.exports = async (req, res, next) => {
+  const surveyId = req.params.id
+  const user = await User.findById(req.user._id)
+
+  if (!user.isAdminOnSurvey(surveyId))
+    return res
+      .status(403)
+      .send('Access denied. this user is not an admin on this survey')
+
+  next()
 }
